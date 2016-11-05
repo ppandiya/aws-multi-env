df2eafe6ff469eb8b5b2f88616ce7516e528993f
Prasanna Pandiyan <sriprasanna.p@gmail.com>
2016-11-05 09:44:45 +0000

    added some other variables temporarily. Need to move it into another file

--- provider-variables.tf
+++ provider-variables.tf
@@ -2,3 +2,6 @@ variable "aws_access_key" {}
 variable "aws_secret_key" {}
 variable "aws_region" {}
 variable "environment_name" {default = "dev"}
+variable "public_key_path" {
+default = "C:/Users/sysadmin/idrsa.pub"
+}
